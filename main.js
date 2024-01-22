5c847408a413559f98ba82427100b3e0118619c4
Hans Maas <hansmaas@Hanss-MacBook-Air.local>
2024-01-23 00:51:37 +0100

    finished header image with all supportive elements

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import "/css/style.css";
+
+const init = () => {
+    document.querySelector('.hamburger').addEventListener('click', () => {
+        document.querySelector('.nav__wrapper').classList.toggle('expanded');
+    });
+}
+
+init();
